4b597cee663058deec8b4c6ef161d6e460a2f1a2
Mohamed Kaddour <kadnakamo@gmail.com>
2024-12-05 17:01:27 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import cowsay from "cowsay";
+
+dotenv.config();
+
+const name = process.env.NAME;
+const campus = process.env.CAMPUS;
+
+console.log(
+	cowsay.say({
+		text: `Hello, I'm Mohamed from Lille!`,
+		e: "oO",
+		T: "U ",
+	}),
+);
